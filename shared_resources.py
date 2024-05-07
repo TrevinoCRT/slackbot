832c28d89c1118d48f559356bb4002b702cc4d7e
Trevino-CRT <ciaranrtrevino@gmail.com>
2024-05-07 11:36:11 -0700

    fixed shared resources file to correctly convert json string

--- shared_resources.py
+++ shared_resources.py
@@ -4,16 +4,24 @@ import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 import json
 import os
-# Convert the string back to a dictionary
-service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
 # Initialize logger
 logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
 
 # Initialize Slack app
 slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+# Fetch the environment variable
+firebase_service_account = os.getenv('FIREBASE_SERVICE_ACCOUNT')
+
+if firebase_service_account is None:
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT environment variable is not set.")
+
+# Convert the string back to a dictionary
+service_account_info = json.loads(firebase_service_account)
+
 # Initialize Firestore DB
 if not firebase_admin._apps:
+    # Pass the dictionary directly to credentials.Certificate
     cred = credentials.Certificate(service_account_info)
     initialize_app(cred)
 db = firestore.client()
