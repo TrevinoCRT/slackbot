4783815c36a76d54a77fe6e50a6b6c32ea161c9e
Trevino-CRT <ciaranrtrevino@gmail.com>
2024-05-07 11:27:29 -0700

    shared resources script

--- shared_resources.py
+++ shared_resources.py
@@ -1,9 +1,11 @@
 from loguru import logger
 from slack_bolt import App
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, initialize_app
+import json
 import os
-
+# Convert the string back to a dictionary
+service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
 # Initialize logger
 logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
 
@@ -12,6 +14,6 @@ slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 # Initialize Firestore DB
 if not firebase_admin._apps:
-    cred = credentials.Certificate("ServiceAccountKey.json")
-    firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate(service_account_info)
+    initialize_app(cred)
 db = firestore.client()
